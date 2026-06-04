0eaafeae74bf2f37c1f0d78dfea9c439973c50d8
sodatax <h.nacanaynay@gmail.com>
2026-06-04 10:41:23 -0700

    finished sumLeafNodes

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -42,7 +42,11 @@ public class TraversalPractice {
      * @return the sum of the leaf node values
      */
     public static int sumLeafNodes(TreeNode node) {
+        if(node==null) return 0;
+        if(node.left == null && node.right==null){
+            return node.data;
+        }
 
-        return -1;
+        return sumLeafNodes(node.left) + sumLeafNodes(node.right);
     }
 }
